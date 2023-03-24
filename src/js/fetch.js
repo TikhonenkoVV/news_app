f7eca2a086434ca2c4cc47057ded05a7b569dfc4
TatianaRohovets <tnr78@ukr.net>
2023-03-24 14:24:24 +0200

    fetch finish

--- src/js/fetch.js
+++ src/js/fetch.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BASE_URL = 'https://api.nytimes.com/svc';
-const API_KEY = '9N64GHLPz8NYmN1YLSHix7rssCU9yIvT';
+const API_KEY = '';
 
 export const fetchPopularArticles = async () => {
     try {
