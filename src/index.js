8aa6cada99162f70e8697ab339db5bdd591e3a09
kozhushna <k.kozhushna@gmail.com>
2023-03-24 16:39:28 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
-import {fetchPopularArticles, fetchSearchArticles, fetchCategoryArticles} from './js/fetch';
+import {
+    fetchPopularArticles,
+    fetchSearchArticles,
+    fetchCategoryArticles,
+} from './js/fetch';
+import * as weatherBanner from './js/weather-banner';
 // console.log(fetchPopularArticles());
 // console.log(fetchSearchArticles(2, 'china'));
 // console.log(fetchCategoryArticles(5, 'arts'));
