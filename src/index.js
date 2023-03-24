eba48edf1a4c86a92f14ca8dab2b11d97492d1c4
TatianaRohovets <tnr78@ukr.net>
2023-03-24 13:40:23 +0200

    fetch done

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import {fetchPopularArticles, fetchSearchArticles, fetchCategoryArticles} from './js/fetch';
 console.log(fetchPopularArticles());
 console.log(fetchSearchArticles(2, 'china'));
-console.log(fetchCategoryArticles(5, 'sport'));
+console.log(fetchCategoryArticles(5, 'arts'));
