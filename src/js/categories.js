d7bcbad5056e1eae2d81342a17a028ff8912a2fd
Danylo Bay <danylobay@gmail.com>
2023-03-26 00:55:44 +0100

    categories
    Відправляю на рев'ю, але є два косячки ще, не змінюється текст контент кнопки на таблеті та декстопі, та при медіа запитах чомусь змінює розмітку тільки при перезагрузці

--- src/js/categories.js
+++ src/js/categories.js
@@ -1,11 +1,34 @@
 import { fetchCategoryArticles } from './fetch';
 
 const categoriesBtns = document.querySelector('.home__category');
-const categoriesDropdown = document.querySelector('.home__category-dropdown');
+const categoriesDropdown = document.querySelector('.home__dropdown-menu');
+
+const categoryButton = document.querySelector('.home__category-button');
+const openIcon = document.querySelector('.home__category-button-icon--open');
+const closeIcon = document.querySelector('.home__category-button-icon--close');
+
+categoryButton.addEventListener('click', () => {
+    openIcon.classList.toggle('hidden');
+    closeIcon.classList.toggle('hidden');
+    categoriesDropdown.classList.toggle('hidden');
+});
+
+const cachedResults = {};
+
+const createBtnsMarkupMobile = results => {
+    categoriesBtns.innerHTML = '';
+
+    const dropdownMarkup = results
+        .map(({ display_name }) => {
+            return `<button class="home__dropdown-item" type="button">${display_name}</button>`;
+        })
+        .join('');
+    categoriesDropdown.insertAdjacentHTML('beforeend', dropdownMarkup);
+};
 
 const createBtnsMarkupTablet = results => {
     const buttonsMarkup = results
-        .slice(0, 3)
+        .slice(0, 4)
         .map(({ display_name }) => {
             return `<button class="home__category-button" type="button">
                     ${display_name}
@@ -14,31 +37,14 @@ const createBtnsMarkupTablet = results => {
         .join('');
 
     const dropdownMarkup = results
-        .slice(3)
+        .slice(4)
         .map(({ display_name }) => {
-            return `<button class="home__dropdown-item" type="button">${display_name}
-                    <svg width="14" height="14">
-                    <use href="./images/sprite.svg#icon-arrow-down"></use>
-                    <use href="./images/sprite.svg#icon-arrow-up"></use>
-                    </svg>
-                </button>`;
+            return `<button class="home__dropdown-item" type="button">${display_name}</button>`;
         })
         .join('');
 
-    return `
-    ${buttonsMarkup}
-    <div class="home__category-dropdown">
-      <button class="home__category-button">Other</button>
-      <div class="home__dropdown-menu">
-        ${dropdownMarkup}
-      </div>
-    </div>
-  `;
-};
-
-const renderMarkupIfTablet = results => {
-    const markupTablet = createBtnsMarkupTablet(results);
-    categoriesBtns.innerHTML = markupTablet;
+    categoriesBtns.innerHTML = buttonsMarkup;
+    categoriesDropdown.insertAdjacentHTML('beforeend', dropdownMarkup);
 };
 
 const createBtnsMarkupDesktop = results => {
@@ -58,34 +64,38 @@ const createBtnsMarkupDesktop = results => {
         })
         .join('');
 
-    // повертаємо маркап кнопок та дропдауна
-    categoriesBtns.insertAdjacentHTML('beforeend', buttonsMarkup);
-    categoriesDropdown.insertAdjacentHTML(
-        'beforeend',
-        ` <div class="home__dropdown-menu">
-        ${dropdownMarkup}
-      </div>`
-    );
+    categoriesBtns.innerHTML = buttonsMarkup;
+    categoriesDropdown.insertAdjacentHTML('beforeend', dropdownMarkup);
 };
-
-// const renderMarkupIfDesktop = results => {
-//     const { buttonsMarkup, dropdownMarkup } = createBtnsMarkupDesktop(results);
-
-//     categoriesBtns.insertAdjacentHTML('beforeend', buttonsMarkup);
-//     categoriesDropdown.insertAdjacentHTML('beforeend', dropdownMarkup);
-// };
 
 const renderMarkup = () => {
     if (window.innerWidth >= 1280) {
-        fetchCategoryArticles().then(({ results }) =>
-            createBtnsMarkupDesktop(results)
-        );
+        if (cachedResults.desktop) {
+            createBtnsMarkupDesktop(cachedResults.desktop);
+        } else {
+            fetchCategoryArticles().then(({ results }) => {
+                createBtnsMarkupDesktop(results);
+                cachedResults.desktop = results;
+            });
+        }
     } else if (window.innerWidth >= 768 && window.innerWidth < 1280) {
-        fetchCategoryArticles().then(({ results }) =>
-            renderMarkupIfTablet(results)
-        );
+        if (cachedResults.tablet) {
+            createBtnsMarkupTablet(cachedResults.tablet);
+        } else {
+            fetchCategoryArticles().then(({ results }) => {
+                createBtnsMarkupTablet(results);
+                cachedResults.tablet = results;
+            });
+        }
     } else {
-        // Викликати функцію для рендерингу мобільного макету
+        if (cachedResults.mobile) {
+            createBtnsMarkupMobile(cachedResults.mobile);
+        } else {
+            fetchCategoryArticles().then(({ results }) => {
+                createBtnsMarkupMobile(results);
+                cachedResults.mobile = results;
+            });
+        }
     }
 };
 
