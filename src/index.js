9f8522f945d99d41a22914fe0eb6b17419015418
TikhonenkoVV <113926273+TikhonenkoVV@users.noreply.github.com>
2023-03-25 18:48:08 +0200

    markup done

--- src/index.js
+++ src/index.js
@@ -5,25 +5,8 @@ import {
 } from './js/fetch';
 import { refs } from './js/refs';
 import { onToglerClick } from './js/togler';
+import { onTabsClick } from './js/on-tabs-click';
 
 refs.togler.addEventListener('click', onToglerClick);
 
-// console.log(fetchPopularArticles());
-// console.log(fetchSearchArticles(2, 'china'));
-// console.log(fetchCategoryArticles(5, 'arts'));
-
-// async function allData() {
-//     const { data } = await fetchPopularArticles();
-//     const { results, num_results } = data;
-//     const users = results
-//         .map(({ published_date, section, abstract, media, title }) => {
-//             let url;
-//             const arr = media[0];
-//             arr ? (url = arr['media-metadata'][0].url) : (url = '');
-//             return `<img src="${url}" />`;
-//         })
-//         .join('');
-//     return console.log(users);
-// }
-
-// allData();
+refs.tabs.addEventListener('click', onTabsClick);
--- src/js/refs.js
+++ src/js/refs.js
@@ -1,3 +1,14 @@
 export const refs = {
     togler: document.querySelector('.theme__togler'),
+    tabs: document.querySelector('.authorization__box'),
+    authorizationSubmit: document.querySelector('.authorization__submit'),
+    regBtn: document.querySelector('.authorization__btn--reg'),
+    logBtn: document.querySelector('.authorization__btn--log'),
+    checkBoxMessage: document.querySelector(
+        '.authorization__checkbox--message'
+    ),
+    checkBoxGeo: document.querySelector(
+        '.authorization__checkbox--geolocation'
+    ),
+    confirmPass: document.querySelector('.authorization__field--confirm'),
 };
