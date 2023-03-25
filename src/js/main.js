ff7e157112d37b514cf1c98a349742942554e776
Oleg833 <oleg.kovtun447@gmail.com>
2023-03-25 05:02:28 +0200

    (window.matchMedia('(min-width: 768px)').matches)

--- src/js/main.js
+++ src/js/main.js
@@ -36,34 +36,64 @@ allData();
 function renderGallery(users) {
     const gallaryMarkup = users
         .map(
-            ({
-                imgUrl,
-                title,
-                section,
-                abstract,
-                published_date,
-                url,
-            }) => `<div class="news__item">
+            (
+                { imgUrl, title, section, abstract, published_date, url },
+                index
+            ) => {
+                if (window.matchMedia('(min-width: 1280px)').matches) {
+                    if (index > 7) {
+                        return;
+                    }
+                } else if (window.matchMedia('(min-width: 768px)').matches) {
+                    if (index > 6) {
+                        return;
+                    }
+                } else {
+                    if (index > 3) {
+                        return;
+                    }
+                }
+
+                return `<div class="news__item">
             <p class="news__section">${section}</p>
             <div class="news__img">
               <img src="${imgUrl}" alt="${title}" loading="lazy"/>
-              <button class="news__btn">Add to favorite<svg class="news__btn-icon" width="20" height="20">
-                  <use href="./images/sprite.svg#icon-heart-border"></use>
-                </svg></button>
-            </div>
+              <button type="button" class="news__btn">Add to favorite
+              <svg class="news__btn-icon" width="20" height="20">
+                <use href="./images/sprite.svg#icon-heart-border"></use>
+                </svg></button></div>
             <div class="info">
               <p class="info__title">${title}</p>
               <p class="info__abstract">${abstract}</p>
               <p class="info__published-date">${published_date}</p>
               <a href="${url}" target="_blank"
-                  rel="noopener noreferrer nofollow" class="info__link">Read more</a>
-            </div></div>`
+                rel="noopener noreferrer nofollow"
+                 class="info__link">Read more</a>
+            </div></div>`;
+            }
         )
         .join('');
 
     // galleryContainer.innerHTML = gallaryMarkup;
     refs.newsContainer.insertAdjacentHTML('beforeend', gallaryMarkup);
 }
+
+`<div class="news__item">
+            <p class="news__section">${section}</p>
+            <div class="news__img">
+              <img src="${imgUrl}" alt="${title}" loading="lazy"/>
+              <button class="news__btn">Add to favorite
+              <svg class="news__btn-icon" width="20" height="20">
+                <use href="./images/sprite.svg#icon-heart-border"></use>
+                </svg></button></div>
+            <div class="info">
+              <p class="info__title">${title}</p>
+              <p class="info__abstract">${abstract}</p>
+              <p class="info__published-date">${published_date}</p>
+              <a href="${url}" target="_blank"
+                rel="noopener noreferrer nofollow"
+                 class="info__link">Read more</a>
+            </div></div>`;
 
 // const tempArr = results.map(
 //     ({ published_date, section, abstract, media, title }) => {
