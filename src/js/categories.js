50fd7130167b5c079206a9321e8ae3bfb2edb28c
Danylo Bay <danylobay@gmail.com>
2023-03-26 14:46:51 +0200

    test response JS

--- src/js/categories.js
+++ src/js/categories.js
@@ -13,17 +13,16 @@ categoryButton.addEventListener('click', () => {
     categoriesDropdown.classList.toggle('hidden');
 });
 
-const cachedResults = {};
-
 const createBtnsMarkupMobile = results => {
-    categoriesBtns.innerHTML = '';
-
-    const dropdownMarkup = results
+    return results
         .map(({ display_name }) => {
-            return `<button class="home__dropdown-item" type="button">${display_name}</button>`;
+            return `<a href="" class="home__dropdown-menu--item"><button class="home__dropdown-item" type="button">${display_name}</button></a>`;
         })
         .join('');
-    categoriesDropdown.insertAdjacentHTML('beforeend', dropdownMarkup);
+};
+
+const renderMarkupMobile = results => {
+    categoriesDropdown.innerHTML = createBtnsMarkupMobile(results);
 };
 
 const createBtnsMarkupTablet = results => {
@@ -39,12 +38,12 @@ const createBtnsMarkupTablet = results => {
     const dropdownMarkup = results
         .slice(4)
         .map(({ display_name }) => {
-            return `<button class="home__dropdown-item" type="button">${display_name}</button>`;
+            return `<a href="" class="home__dropdown-menu--item"><button class="home__dropdown-item" type="button">${display_name}</button></a>`;
         })
         .join('');
 
     categoriesBtns.innerHTML = buttonsMarkup;
-    categoriesDropdown.insertAdjacentHTML('beforeend', dropdownMarkup);
+    categoriesDropdown.innerHTML = dropdownMarkup;
 };
 
 const createBtnsMarkupDesktop = results => {
@@ -60,43 +59,94 @@ const createBtnsMarkupDesktop = results => {
     const dropdownMarkup = results
         .slice(6) // обмежуємо результат всіма категоріями, що залишилися
         .map(({ display_name }) => {
-            return `<button class="home__dropdown-item" type="button">${display_name}</button>`;
+            return `<a href="" class="home__dropdown-menu--item"><button class="home__dropdown-item" type="button">${display_name}</button></a>`;
         })
         .join('');
 
     categoriesBtns.innerHTML = buttonsMarkup;
-    categoriesDropdown.insertAdjacentHTML('beforeend', dropdownMarkup);
+    categoriesDropdown.innerHTML = dropdownMarkup;
 };
 
-const renderMarkup = () => {
-    if (window.innerWidth >= 1280) {
-        if (cachedResults.desktop) {
-            createBtnsMarkupDesktop(cachedResults.desktop);
-        } else {
+let timeout;
+
+window.addEventListener('resize', () => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+        const screenWidth = window.innerWidth;
+        if (screenWidth < 768) {
+            // mobile
+            console.log('<768px');
+            categoriesBtns.innerHTML = '';
             fetchCategoryArticles().then(({ results }) => {
-                createBtnsMarkupDesktop(results);
-                cachedResults.desktop = results;
+                renderMarkupMobile(results);
             });
-        }
-    } else if (window.innerWidth >= 768 && window.innerWidth < 1280) {
-        if (cachedResults.tablet) {
-            createBtnsMarkupTablet(cachedResults.tablet);
-        } else {
+        } else if (screenWidth < 1280) {
+            // tablet
+            console.log('>768px');
+            categoriesBtns.innerHTML = '';
             fetchCategoryArticles().then(({ results }) => {
                 createBtnsMarkupTablet(results);
-                cachedResults.tablet = results;
             });
-        }
-    } else {
-        if (cachedResults.mobile) {
-            createBtnsMarkupMobile(cachedResults.mobile);
         } else {
+            // desktop
+            console.log('>1280px');
+            categoriesBtns.innerHTML = '';
             fetchCategoryArticles().then(({ results }) => {
-                createBtnsMarkupMobile(results);
-                cachedResults.mobile = results;
+                createBtnsMarkupDesktop(results);
             });
         }
-    }
-};
+    }, 500);
+});
 
-window.addEventListener('load', renderMarkup);
+// if (matchMedia) {
+//     const screenMobile = window.matchMedia('(min-width:320px)');
+//     screenMobile.addListener(changesMobile);
+//     changesMobile(screenMobile);
+// }
+
+// function changesMobile(screenMobile) {
+//     if (screenMobile.matches) {
+//         console.log('<768px');
+//         categoriesBtns.innerHTML = '';
+//         fetchCategoryArticles().then(({ results }) => {
+//             renderMarkupMobile(results);
+//         });
+//     }
+// }
+
+// if (matchMedia) {
+//     const screenTablet = window.matchMedia('(min-width:768px)');
+//     screenTablet.addListener(changesTablet);
+//     changesTablet(screenTablet);
+// }
+
+// function changesTablet(screenTablet) {
+//     if (screenTablet.matches) {
+//         console.log('>768px');
+//         categoriesBtns.innerHTML = '';
+//         fetchCategoryArticles().then(({ results }) => {
+//             createBtnsMarkupTablet(results);
+//         });
+//     }
+// }
+
+// if (matchMedia) {
+//     const screenDesktop = window.matchMedia('(min-width:1280px)');
+//     screenDesktop.addListener(changesDesktop);
+//     changesDesktop(screenDesktop);
+// }
+
+// function changesDesktop(screenDesktop) {
+//     if (screenDesktop.matches) {
+//         console.log('>1280px');
+//         categoriesBtns.innerHTML = '';
+//         fetchCategoryArticles().then(({ results }) => {
+//             createBtnsMarkupDesktop(results);
+//         });
+//     } else {
+//         categoriesBtns.innerHTML = '';
+//         fetchCategoryArticles().then(({ results }) => {
+//             createBtnsMarkupTablet(results);
+//         });
+//     }
+// }
