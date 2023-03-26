8882649f70e0c5d7a10dc6061629bec3494ce227
Sophia <sophia@MacBook-Air-Sophia.local>
2023-03-26 14:39:38 +0300

    added search form and active nav link

--- src/index.js
+++ src/index.js
@@ -4,18 +4,19 @@ import {
     fetchSearchArticles,
     fetchCategoryArticles,
 } from './js/fetch';
+import './js/searchForm';
 // console.log(fetchPopularArticles());
 // console.log(fetchSearchArticles(2, 'china'));
 // console.log(fetchCategoryArticles(5, 'arts'));
 import { refs } from './js/refs';
 import { onToglerClick, checkCurrentTheme } from './js/togler';
-checkCurrentTheme();
-refs.togler.addEventListener('click', onToglerClick);
-refs.mobileToggler.addEventListener('click', onToglerClick);
 import { refs } from './js/refs';
 import { onToglerClick } from './js/togler';
 import { onTabsClick } from './js/on-tabs-click';
 import * as weatherBanner from './js/weather-banner';
 
+checkCurrentTheme();
+refs.mobileToggler.addEventListener('click', onToglerClick);
 refs.togler.addEventListener('click', onToglerClick);
 refs.tabs.addEventListener('click', onTabsClick);
+console.log();
--- src/js/togler.js
+++ src/js/togler.js
@@ -30,10 +30,59 @@ export function setTheme(isDarkTheme) {
 
 export function checkCurrentTheme() {
     const theme = localStorage.getItem('theme');
+    const currentLocation = window.location.pathname;
+    const navLinks = document.querySelectorAll('.site-nav__link');
+    const mobileLinks = document.querySelectorAll('.mobile-menu__item');
+    if (currentLocation.includes('favorite')) {
+        toggleNavigationMenuClasses(navLinks, mobileLinks, 'favorite');
+    } else if (currentLocation.includes('read')) {
+        toggleNavigationMenuClasses(navLinks, mobileLinks, 'read');
+    } else {
+        toggleNavigationMenuClasses(navLinks, mobileLinks, 'home');
+    }
     if (theme === 'dark') {
         onToglerClick();
         return;
     }
+}
+
+function toggleNavigationMenuClasses(navLinks, mobileNavLinks, page) {
+    [...navLinks].forEach((item, index) => {
+        if (item.textContent.toLowerCase() === page) {
+            item.classList.add('site-nav__link--active');
+            mobileNavLinks[index].classList.add('mobile-menu__item--active');
+            mobileNavLinks[index]
+                .querySelector('.mobile-menu__link')
+                .classList.add('mobile-menu__link--active');
+            mobileNavLinks[index]
+                .querySelector('.mobile-menu__item-svg')
+                .classList.add('mobile-menu__item-svg--active');
+            mobileNavLinks[index]
+                .querySelector('.mobile-menu__item-next-svg')
+                ?.classList.replace(
+                    'mobile-menu__item-next-svg',
+                    'mobile-menu__item-next-svg--active'
+                );
+            return;
+        }
+        item.classList.remove('site-nav__link--active');
+        mobileNavLinks[index].classList.remove('mobile-menu__item--active');
+        mobileNavLinks[index]
+            .querySelector('.mobile-menu__link')
+            .classList.remove('mobile-menu__link--active');
+        mobileNavLinks[index]
+            .querySelector('.mobile-menu__item-svg')
+            .classList.remove('mobile-menu__link--active');
+        mobileNavLinks[index]
+            .querySelector('.mobile-menu__item-svg')
+            .classList.remove('mobile-menu__item-svg--active');
+        mobileNavLinks[index]
+            .querySelector('.mobile-menu__item-next-svg--active')
+            ?.classList.replace(
+                'mobile-menu__item-next-svg--active',
+                'mobile-menu__item-next-svg'
+            );
+    });
 }
 
 function changeRefStyle(ref, ...styles) {
