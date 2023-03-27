245e776fc9b48adc955a4cf13aa4d2540fa243d8
Sophia <sophia@MacBook-Air-Sophia.local>
2023-03-27 19:36:39 +0300

    fixed problems

--- src/index.js
+++ src/index.js
@@ -1,24 +1,25 @@
-import './js/mobile-menu';
 import './js/searchForm';
+import './js/mobile-menu';
 import { refs } from './js/refs';
-import { onToglerClick } from './js/togler';
+import { onToglerClick, checkCurrentTheme } from './js/togler';
 import { refs } from './js/refs';
 import { onToglerClick } from './js/togler';
 import { onTabsClick } from './js/on-tabs-click';
 import { onClickBtns, onClickBtnsDropdown } from './js/categories-filter';
+import * as weatherBanner from './js/weather-banner';
+import { allData } from './js/main';
 import { handleScreenSizeChange } from './js/categories';
 
 refs.mobileToggler.addEventListener('click', onToglerClick);
 refs.togler.addEventListener('click', onToglerClick);
 refs.tabs.addEventListener('click', onTabsClick);
 
+checkCurrentTheme();
 handleScreenSizeChange();
 
-refs.categoriesBtns.addEventListener('click', onClickBtns);
-refs.categoriesDropdown.addEventListener('click', onClickBtnsDropdown);
-
-import * as weatherBanner from './js/weather-banner';
-
-import { allData } from './js/main';
+if (refs.categoriesBtns && refs.categoriesDropdown) {
+    refs.categoriesBtns.addEventListener('click', onClickBtns);
+    refs.categoriesDropdown.addEventListener('click', onClickBtnsDropdown);
+}
 
 allData();
