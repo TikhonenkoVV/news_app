03b753e804dd87d4a383a2fae9fcf1ea19e05d91
TikhonenkoVV <113926273+TikhonenkoVV@users.noreply.github.com>
2023-03-28 10:31:52 +0300

    fix weather

--- src/js/read/addremove-data-favorite.js
+++ src/js/read/addremove-data-favorite.js
@@ -13,31 +13,31 @@ export const addRemoveDataFavorite = e => {
 
     if (userGallery) {
         const array = userGallery.map(obj => {
-           if (obj.url !== url) return obj;
-           obj.favorite = !obj.favorite;
-           return obj;
+            if (obj.url !== url) return obj;
+            obj.favorite = !obj.favorite;
+            return obj;
         });
-        localStorage.setItem("user-gallery", JSON.stringify(array)); 
+        localStorage.setItem('user-gallery', JSON.stringify(array));
         const index = userGallery.findIndex(obj => url === obj.url);
         if (index !== -1) return;
-    };
+    }
 
     const savedLocalNews = localStorage.getItem('bite-search');
     JSON.parse(savedLocalNews).map(fetchNew => {
         if (url !== fetchNew.url) return;
         console.log(url);
         console.log(fetchNew.url);
-    const url = e.target.id;
-    const results = JSON.parse(savedLocalNews).map(fetchNew => {
-        if (url !== fetchNew.url) return fetchNew;
-        if (fetchNew.favorite === true) {
-            fetchNew.favorite = false;
-        }
-        fetchNew.favorite = true;
-        newArr.push(fetchNew);
-        localStorage.setItem("user-gallery", JSON.stringify(newArr));
-});
-};
+        const url = e.target.id;
+        const results = JSON.parse(savedLocalNews).map(fetchNew => {
+            if (url !== fetchNew.url) return fetchNew;
+            if (fetchNew.favorite === true) {
+                fetchNew.favorite = false;
+            }
+            fetchNew.favorite = true;
+            newArr.push(fetchNew);
+            localStorage.setItem('user-gallery', JSON.stringify(newArr));
+        });
+        // };
         e.target.innerHTML = `Remove from favorite<svg class="news__btn-icon" width="20" height="20"><use href="#icon-heart-fill"></use></svg>`;
         console.log(e.target.textContent);
         return fetchNew;
