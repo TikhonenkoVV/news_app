cd3902c94e611d9b3700eb866900fa8ae7fbed2d
TikhonenkoVV <113926273+TikhonenkoVV@users.noreply.github.com>
2023-03-29 14:01:15 +0300

    commit

--- src/js/read/read.js
+++ src/js/read/read.js
@@ -1,15 +1,13 @@
-import { renderGalleryReadOnDays } from "./render-markup-read";
-import { sortReadNewsData } from "./sort-news-data";
-import { addDataReadNews } from "./add-data-read-more";
-import { refs } from "../refs";
-import { fetchArrayWithDBReedNews } from '../autorization';
+import { renderGalleryReadOnDays } from './render-markup-read';
+import { sortReadNewsData } from './sort-news-data';
+import { addDataReadNews } from './add-data-read-more';
+import { refs } from '../refs';
+import { verifyUser, fetchArrayWithDBReedNews } from '../autorization';
 
+verifyUser();
 
 renderGalleryReadOnDays();
 
 refs.boxItems.addEventListener('click', addDataReadNews);
 const array = sortReadNewsData();
 renderGalleryReadOnDays(array);
-
-
-
