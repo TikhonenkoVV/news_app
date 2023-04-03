5428b8af0e2289372868bceec5d573870e0ccc9f
Vitaliy Tikhonenko <113926273+TikhonenkoVV@users.noreply.github.com>
2023-04-03 03:58:36 +0300

    Delete src directory

--- src/js/check-current-location.js
+++
@@ -1,6 +0,0 @@
-export const checkCurrentLocation = () => {
-    const currentLocation = window.location.pathname;
-    if (currentLocation.includes('favorite')) return 'favorite';
-    if (currentLocation.includes('favorite')) return 'read';
-    return 'index';
-};
